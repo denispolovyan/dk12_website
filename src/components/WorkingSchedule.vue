c0a520b3a6e8c621e5188891ea4365d3bd82810f
denispolovyan <denispolovyan@gmail.com>
2023-02-18 00:38:27 +0200

    todo list start

--- src/components/WorkingSchedule.vue
+++ src/components/WorkingSchedule.vue
@@ -123,6 +123,59 @@
                   @click="addScroll"
                   class="data__close"
                 ></div>
+                <div class="todo-list">
+                  <div class="todo-list__body">
+                    <div class="todo-list__title">
+                      <input
+                        id="taskTitle"
+                        type="text"
+                        placeholder="Title of task"
+                        maxlength="20"
+                      />
+                    </div>
+                    <div class="todo__list__text">
+                      <textarea
+                        id="taskBody"
+                        placeholder="Body of task"
+                        maxlength="100"
+                      ></textarea>
+                    </div>
+                    <button
+                      class="todo-list__button todo-list__button_submit"
+                      @click="addTodo"
+                    >
+                      Submit
+                    </button>
+                    <button
+                      class="todo-list__button todo-list__button_reset"
+                      @click="resetForm"
+                    >
+                      Reset
+                    </button>
+                  </div>
+                </div>
+                <div class="card__container_todo">
+                  <div
+                    class="card__item_todo"
+                    v-for="todo in todoList"
+                    :key="todo.name"
+                  >
+                    <div class="card__body">
+                      <div class="card__title_todo">{{ todo.name }}</div>
+                      <div class="card__text_todo">
+                        {{ todo.text }}
+                      </div>
+                      <button
+                        class="todo-list__button todo-list__button_submit"
+                      >
+                        Overline
+                      </button>
+                      <button class="todo-list__button todo-list__button_reset">
+                        Delete
+                      </button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
             <div class="card__subtitle">8:30</div>
@@ -623,6 +676,85 @@
   border-radius: 20px;
 }
 
+/* todo list  */
+.todo-list {
+  top: 100px;
+  left: 20px;
+  position: absolute;
+  background-color: #f5deb3;
+  border-radius: 20px;
+}
+.todo-list__body {
+  padding: 20px;
+}
+.todo-list__title {
+  margin: 0px 0px 40px 0px;
+}
+.todo-list__title input {
+  font-size: 18px;
+  padding: 0px 20px;
+  border-radius: 20px;
+  width: 300px;
+  height: 40px;
+}
+.todo__list__text textarea {
+  font-size: 18px;
+  padding: 20px 20px;
+  border-radius: 20px;
+  width: 300px;
+  height: 150px;
+  resize: none;
+}
+.todo-list__button {
+  font-size: 18px;
+  padding: 10px 15px;
+  border-radius: 20px;
+  margin: 20px 0px 0px 0px;
+}
+.todo-list__button_submit {
+  background-color: #f0fff0;
+  margin: 0px 20px 0px 0px;
+}
+.todo-list__button_reset {
+  background-color: #fff5ee;
+}
+.todo-list__button_submit:hover {
+  transition-duration: 0.7s;
+  background-color: #98fb98;
+  margin: 0px 20px 0px 0px;
+}
+.todo-list__button_reset:hover {
+  transition-duration: 0.7s;
+  background-color: #ffa07a;
+}
+.card__container_todo {
+  position: absolute;
+  left: 20px;
+  top: 500px;
+  width: 340px;
+}
+
+/* todo card  */
+.card__item_todo {
+  padding: 20px;
+  background-color: #f5deb3;
+  border-radius: 20px;
+  margin: 0px 0px 20px 0px;
+}
+
+.card__title_todo {
+  padding: 10px 15px;
+  background-color: #fff;
+  margin: 0px 0px 20px 0px;
+  border-radius: 20px;
+}
+.card__text_todo {
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 20px;
+  line-height: 20px;
+}
+
 /* media  */
 
 @media (min-width: 2300px) {
@@ -646,7 +778,8 @@ export default {
         thursday: false,
         friday: false,
       },
-      clientWidth: 0,
+      clientWidth: "",
+      todoList: [],
     };
   },
   methods: {
@@ -678,7 +811,7 @@ export default {
       let element = e.target.id;
       this.days[element] = true;
     },
-    handleWindowResize() {
+    getWindowSize() {
       this.clientWidth = window.screen.width;
     },
     removeScroll() {
@@ -687,10 +820,36 @@ export default {
     addScroll() {
       document.body.style.overflow = "scroll";
     },
+    resetForm() {
+      document.getElementById("taskBody").value = "";
+      document.getElementById("taskTitle").value = "";
+    },
+    addTodo() {
+      const title = document.getElementById("taskTitle").value;
+      const body = document.getElementById("taskBody").value;
+      if (title && body) {
+        const currentTodo = {
+          name: title,
+          text: body,
+        };
+        this.todoList = [...this.todoList, currentTodo];
+      }
+      this.resetForm();
+    },
+  },
+  computed: {
+    returnTodos() {
+      return this.todoList;
+    },
   },
   mounted() {
     document.addEventListener("keydown", this.handleKeydown);
-    window.addEventListener("resize", this.handleWindowResize);
+    window.addEventListener("resize", this.getWindowSize);
+    this.getWindowSize();
+  },
+  beforeUnmount() {
+    document.removeEventListener("keydown", this.handleKeydown);
+    window.removeEventListener("resize", this.getWindowSize);
   },
 };
 </script>
