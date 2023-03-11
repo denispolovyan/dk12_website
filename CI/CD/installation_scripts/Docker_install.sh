8b212de2fa721b39dd03ed54ee3ceb3a506d7924
barokoVlad <mailbrovko@gmail.com>
2023-03-11 16:50:08 +0200

    fix: fixed Docker_install.sh

--- CI/CD/installation_scripts/Docker_install.sh
+++ CI/CD/installation_scripts/Docker_install.sh
@@ -6,7 +6,7 @@ sudo apt-get install apt-transport-https
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 # Додати репозиторій до списку джерел пакетів
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 # Оновлюємось і встановлюємо DOCKER
 sudo apt-get update
