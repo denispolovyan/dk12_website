5aabfeaac0d0e4f99bcfff209429ff61be139b4d
barokoVlad <mailbrovko@gmail.com>
2023-03-14 18:59:50 +0200

    fix: fixed Npm_prod.sh

--- CI/CD/installation_scripts/Npm_prod.sh
+++ CI/CD/installation_scripts/Npm_prod.sh
@@ -4,11 +4,6 @@ sudo apt-get update
 
 sudo apt-get install -y npm
 
-sudo npm i 
-sudo npm i -g @vue/cli
-sudo npm i -g @vue/cli-service
-sudo npm update -g
-
 cd /home/ubuntu/dk12_website/ 
 
 sudo npm run build
