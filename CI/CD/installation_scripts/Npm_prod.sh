99deb7f50549274f8fc94ebaae2ed79ac37923b4
barokoVlad <mailbrovko@gmail.com>
2023-03-14 18:16:29 +0200

    fix: fixed Npm_prod.sh

--- CI/CD/installation_scripts/Npm_prod.sh
+++ CI/CD/installation_scripts/Npm_prod.sh
@@ -1,8 +1,13 @@
-#!/bin/bash/
+#!/bin/bash
 
 sudo apt-get update
+
 sudo apt-get install -y npm
+
 npm i @vue/cli-service
+
 cd /home/ubuntu/dk12_website/ 
+
 npm run build
+
 mv /home/ubuntu/dk12_website/CI/CD/Dockerfile /home/ubuntu/Dockerfile
